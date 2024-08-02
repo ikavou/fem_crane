96bf0c33d60a1ef528c102d4b42754e4a436cd70
ikavou <144719434+ikavou@users.noreply.github.com>
2024-08-02 15:15:16 +0300

    Initial Commit

---
+++ src/AEM6318_crane_bar.py
@@ -0,0 +1,8 @@
+#Ioannis Kavoukis, 6318
+#Bar Crane Full Software 
+
+exec(open("AEM6318_crane_bar_preprocessor.py").read())
+
+exec(open("AEM6318_crane_bar_solver.py").read())
+
+exec(open("AEM6318_crane_bar_postprocessor.py").read())
